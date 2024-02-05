6f09a18c793cfe7edbe955bae75dd6f568fa3bc2
KeerthigaYoganathan <131315071+KeerthigaYoganathan@users.noreply.github.com>
2024-02-04 20:28:01 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// console.log("Hello");
+// let a = "madam";
+// let b="";
+// for(let i= a.length-1; i>=0; i--){
+//     b= b+a[i];
+// }
+// console.log(b);
+// if(a==b){
+//     console.log("Palindrome");
+// }
+// else{
+//     console.log("Not a palindrome");
+// }
+
+let text = "a,b,c,d,e,f";
+const myArray = text.split(",");
+console.log(myArray);
